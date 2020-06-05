5edc7e42dca7802c5ff468b3b8d35ff2d67b030b
Andrew Fang <47884051+afandrew@users.noreply.github.com>
2020-06-05 10:00:20 -0400

    Update my_dataset.py

--- mmdet/datasets/my_dataset.py
+++ mmdet/datasets/my_dataset.py
@@ -1,7 +1,8 @@
 from .coco import CocoDataset
-from .registry import DATASETS
+from mmdet.utils import Registry
 from .xml_style import XMLDataset
 
+DATASETS = Registry('dataset')
 @DATASETS.register_module
 class MyDataset(XMLDataset):
 
